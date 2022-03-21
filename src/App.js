ac4ba0a838501403fc58615615cdb46155f8b602
yaliilyaya <yaliilyaya@gmai.ru>
2022-03-22 00:37:34 +0300

    adminlte-2-react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import AdminLTE, { Sidebar, Content, Row, Col, Box, Button } from 'adminlte-2-react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const { Item } = Sidebar;
+
+class HelloWorld extends Component {
+  state = {}
+
+  render() {
+    return (<Content title="Hello World" subTitle="Getting started with adminlte-2-react" browserTitle="Hello World">
+      <Row>
+        <Col xs={6}>
+          <Box title="My first box" type="primary" closable collapsable footer={<Button type="danger" text="Danger Button" />}>
+            Hello World
+          </Box>
+        </Col>
+        <Col xs={6}>
+          <Box title="Another box">
+            Content goes here
+          </Box>
+        </Col>
+      </Row>
+    </Content>);
+  }
+}
+
+class App extends Component {
+
+  sidebar = [
+    <Item key="hello" text="Hello" to="/hello-world" />
+  ]
+
+  render() {
+    return (
+        <AdminLTE title={["Hello", "World"]} titleShort={["He", "we"]} theme="blue" sidebar={this.sidebar}>
+          <HelloWorld path="/hello-world" />
+        </AdminLTE>
+    );
+  }
 }
 
 export default App;
